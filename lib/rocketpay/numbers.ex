adfd0457f714ed37cf642522fe80600623b3d5eb
LuizFerK <luizfernandoklein@live.com>
2021-02-22 11:12:39 -0300

    feat: Route params

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -9,10 +9,10 @@ defmodule Rocketpay.Numbers do
     result =
       result
       |> String.split(",")
-      |> Enum.map(fn number -> String.to_integer(number) end)
+      |> Stream.map(fn number -> String.to_integer(number) end)
       |> Enum.sum()
 
     {:ok, %{result: result}}
   end
-  defp handle_file({:error, _reason}), do: {:error, {"Invalid file!"}}
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid file!"}}
 end
