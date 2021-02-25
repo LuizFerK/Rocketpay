9b606964b81cdd55fd7dd69476b56f69a66ded8f
LuizFerK <luizfernandoklein@live.com>
2021-02-25 15:23:25 -0300

    feat: Transaction

--- lib/rocketpay/accounts/withdraw.ex
+++ lib/rocketpay/accounts/withdraw.ex
@@ -1,16 +1,16 @@
-defmodule Rocketpay.Accounts.Deposit do
+defmodule Rocketpay.Accounts.Withdraw do
   alias Rocketpay.{Accounts.Operation, Repo}
 
   def call(params) do
     params
-    |> Operation.call(:deposit)
+    |> Operation.call(:withdraw)
     |> run_transaction()
   end
 
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
       {:error, _operation, reason, _changes} -> {:error, reason}
-      {:ok, %{account_deposit: account}} -> {:ok, account}
+      {:ok, %{account_withdraw: account}} -> {:ok, account}
     end
   end
 end
